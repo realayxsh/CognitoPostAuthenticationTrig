525494f384d2aa27fa98df16b80bcb53c84bb5ac
realayxsh <realayxsh@gmail.com>
2026-05-15 02:26:56 +0530

    Update sharding.js

--- src/sharding.js
+++ src/sharding.js
@@ -10,5 +10,10 @@ const manager = new ClusterManager('./src/index.js', {
 });
 
 manager.spawn({delay: 10000, timeout: -1, amount: manager.totalShards});
-manager.on('clusterCreate', cluster => { console.log([CLUSTER] => Cluster Launched ${cluster.id}) });
-manager.on("clusterReady", cluster => { console.log([CLUSTER] => Cluster is Ready ${cluster.id}) });
+manager.on('clusterCreate', (cluster) => { 
+    console.log([CLUSTER] => Cluster Launched ${cluster.id});
+});
+
+manager.on('clusterReady', (cluster) => { 
+    console.log([CLUSTER] => Cluster is Ready ${cluster.id});
+});
