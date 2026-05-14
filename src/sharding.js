170bb1c7a595550bf6dc71b114cead09e17f96af
realayxsh <realayxsh@gmail.com>
2026-05-15 02:16:35 +0530

    Update sharding.js

--- src/sharding.js
+++ src/sharding.js
@@ -1,13 +1,14 @@
+require('dotenv').config(); // Add this to the very top
 const { ClusterManager } = require("discord-hybrid-sharding");
-const { token } = require(`../config.json`);
 
-const manager = new ClusterManager(`./src/index.js`,{
-    token : token,
-    totalClusters : 2,
-    totalShards : 10,
-    shardsPerClusters : 5
+// We no longer require the config.json for the token
+const manager = new ClusterManager(./src/index.js, {
+    token: process.env.DISCORD_TOKEN, // Changed this line
+    totalClusters: 2,
+    totalShards: 10,
+    shardsPerClusters: 5
 });
 
-manager.spawn({delay : 10000,timeout :-1,amount : manager.totalShards});
-manager.on('clusterCreate',cluster => { console.log(`[CLUSTER] => Cluster Launched ${cluster.id}`)});
-manager.on("clusterReady",cluster => { console.log(`[CLUSTER] => Cluster is Ready ${cluster.id}`)});
+manager.spawn({delay: 10000, timeout: -1, amount: manager.totalShards});
+manager.on('clusterCreate', cluster => { console.log([CLUSTER] => Cluster Launched ${cluster.id}) });
+manager.on("clusterReady", cluster => { console.log([CLUSTER] => Cluster is Ready ${cluster.id}) });
